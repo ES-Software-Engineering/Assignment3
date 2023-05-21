774e19cfa6e55bff09baf8489ad18a3d5b8c7fd0
lenic0411 <lenic13579@gmail.com>
2023-05-21 22:24:19 +0900

    백승호 작업 업로드

--- jobInformationSystem.cpp
+++ jobInformationSystem.cpp
@@ -1,17 +1,46 @@
 //
 // Created by 김성민 on 2023/05/21.
 //
+#include <iostream>
 
 #include "jobInformationSystem.h"
 
 
-void JobInformationSystem::JobPostingRegistration() {
+void JobInformationSystem::JobPostingRegistration(ifstream& fin, ofstream& fout) {
+    fout << "3.1. 채용 정보 등록" << endl;
 
+    string work;
+    int numOfPeople;
+    string deadline;
+
+    fin >> work >> numOfPeople >> deadline;
+    fout << "> " << work << ' ' << numOfPeople << ' ' << deadline << endl;
+
+    JobInformation newJobInformation;
+    newJobInformation.ownerNum = authSystem.getAccountInfo().id;
+    newJobInformation.companyName = authSystem.getAccountInfo().companyName;
+    newJobInformation.businessNum = authSystem.getAccountInfo().businessNum;
+    newJobInformation.work = work;
+    newJobInformation.numOfPeople = numOfPeople;
+    newJobInformation.deadline = deadline;
+
+    jobInformations.push_back(newJobInformation);
 }
 
-void JobInformationSystem::MyJobPosting() {
 
+void JobInformationSystem::MyJobPosting(ifstream& fin, ofstream& fout) {
+    fout << "3.2. 등록된 채용 정보 조회" << endl;
+
+    for (const JobInformation& jobInfo : jobInformations) {
+        if (jobInfo.ownerNum == authSystem.getAccountInfo().id) {
+            fout << "> " << jobInfo.work << ' ' << jobInfo.numOfPeople << ' ' << jobInfo.deadline << endl;
+        }
+    }
 }
+
+
+
+
 
 JobInformation JobInformationSystem::Search() {
 
