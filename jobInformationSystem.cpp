fdf5e13a36670b63c1c1f8df389e151619d193d3
kimsungkyum <sungkyum1@naver.com>
2023-05-22 20:54:10 +0900

    feat: 채용 정보 등록, 등록된 채용 정보 조회 구현

--- jobInformationSystem.cpp
+++ jobInformationSystem.cpp
@@ -5,15 +5,46 @@
 #include "jobInformationSystem.h"
 
 
-void JobInformationSystem::JobPostingRegistration() {
 
+
+void JobInformationSystem::JobPostingRegistration(ifstream& fin, ofstream& fout, AuthSystem authSystem) {
+    fout << "3.1. 채용 정보 등록" << endl;
+
+    string work;
+    int numOfPeople;
+    string deadline;
+
+    fin >> work >> numOfPeople >> deadline;
+    fout << "> " << work << ' ' << numOfPeople << ' ' << deadline << endl;
+
+    JobInformation newJobInformation(
+        authSystem.getAccountInfo().id, 
+        authSystem.getAccountInfo().companyName,
+        authSystem.getAccountInfo().businessNum,
+        work,
+        numOfPeople,
+        deadline
+    );
+
+    jobInformations.push_back(newJobInformation);
 }
 
-void JobInformationSystem::MyJobPosting() {
+void JobInformationSystem::MyJobPosting(ifstream& fin, ofstream& fout, AuthSystem authSystem) {
+    fout << "3.2. 등록된 채용 정보 조회" << endl;
 
+    for (auto jobInfo : jobInformations) {
+        if (jobInfo.getOwnerNum() == authSystem.getAccountInfo().id) {
+            fout << "> " << jobInfo.getWork() << ' ' << jobInfo.getNumOfPeople() << ' ' << jobInfo.getDeadline() << endl;
+        }
+    }
 }
 
-JobInformation JobInformationSystem::Search() {
+JobInformation JobInformationSystem::Search(ifstream& fin, ofstream& fout, AuthSystem authSystem) {
+    // fout << "4.1 로그인" << endl;
+
+    // string id;
+    // fin >> id;
+    // authSystem.getAccountInfo();
 
 }
 
--- jobInformationSystem.h
+++ jobInformationSystem.h
@@ -7,16 +7,84 @@
 
 #include <vector>
 #include <string>
+#include <fstream>
+#include <iostream>
+
+#include "authSystem.h"
 
 using namespace std;
 
 class JobInformation {
-    int ownerNum; // 작성자 ID
+private:
+    string ownerNum; // 작성자 ID
     string companyName; // 회사 이름
     int businessNum; // 사업자 번호
     string work; // 업무
     int numOfPeople; // 인원 수
-    int deadline; // 신청 마감일
+    string deadline; // 신청 마감일
+public: 
+    JobInformation(string ownerNum, string companyName, int businessNum, string work, int numOfPeople, string deadline) {
+        this->ownerNum = ownerNum;
+        this->companyName = companyName;
+        this->businessNum = businessNum;
+        this->work = work;
+        this->numOfPeople = numOfPeople;
+        this->deadline = deadline;
+    };
+    // ownerNum의 getter와 setter
+    string getOwnerNum() const {
+        return ownerNum;
+    }
+
+    void setOwnerNum(const string& owner) {
+        ownerNum = owner;
+    }
+
+    // companyName의 getter와 setter
+    string getCompanyName() const {
+        return companyName;
+    }
+
+    void setCompanyName(const string& company) {
+        companyName = company;
+    }
+
+    // businessNum의 getter와 setter
+    int getBusinessNum() const {
+        return businessNum;
+    }
+
+    void setBusinessNum(int business) {
+        businessNum = business;
+    }
+
+    // work의 getter와 setter
+    string getWork() const {
+        return work;
+    }
+
+    void setWork(const string& job) {
+        work = job;
+    }
+
+    // numOfPeople의 getter와 setter
+    int getNumOfPeople() const {
+        return numOfPeople;
+    }
+
+    void setNumOfPeople(int numPeople) {
+        numOfPeople = numPeople;
+    }
+
+    // deadline의 getter와 setter
+    string getDeadline() const {
+        return deadline;
+    }
+
+    void setDeadline(const string& dueDate) {
+        deadline = dueDate;
+    }
+
 };
 
 class JobInformationSystem {
@@ -24,9 +92,9 @@ private:
     vector<JobInformation> jobInformations; // key
 
 public:
-    void JobPostingRegistration();
-    void MyJobPosting();
-    JobInformation Search();
+    void JobPostingRegistration(ifstream& fin, ofstream& fout, AuthSystem authSystem);
+    void MyJobPosting(ifstream& fin, ofstream& fout, AuthSystem authSystem);
+    JobInformation Search(ifstream& fin, ofstream& fout, AuthSystem authSystem);
     void Apply();
     void ApplyInfo();
     void WithdrawApply();
